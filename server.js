a4889e18539f9b5d0fb5c3d068e1763e330fe156
tsunametuto <tsunametuto@gmail.com>
2026-01-29 12:14:23 -0300

    Update server.js

--- server.js
+++ server.js
@@ -76,6 +76,10 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("ITO Online rodando na porta 3000");
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`ITO Online rodando na porta ${PORT}`);
 });
+
+
